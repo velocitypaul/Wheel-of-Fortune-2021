16b0be5e41a84356a5d0cf6e13fee6c653cc966e
velocitypaul <68206388+velocitypaul@users.noreply.github.com>
2021-04-03 15:17:27 -0600

    Added main area, wheel, puzzle-grid and images.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/wheel_of_fortune_small_logo.png';
+import './images/wheel_fortune_circle.png';
 
 console.log('This is the JavaScript entry file - your code begins here.');
