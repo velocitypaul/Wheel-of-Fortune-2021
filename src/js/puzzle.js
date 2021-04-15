95696a493ed95c99eaea0ab3f6e8065dd7ad25e3
velocitypaul <68206388+velocitypaul@users.noreply.github.com>
2021-04-15 02:47:05 +0000

    Adding puzzle.js

---
+++ src/js/puzzle.js
@@ -0,0 +1,11 @@
+export default class Puzzle {
+	  constructor( category, number_of_words, total_number_of_letters, first_word, description, correct_answer) {
+		  this.category = category;
+		  this.number_of_words = number_of_words;
+		  this.total_number_of_letters = total_number_of_letters;
+		  this.first_word = first_word;
+		  this.description = description;
+		  this.correct_answer = correct_answer;
+	  }
+	  
+	}
