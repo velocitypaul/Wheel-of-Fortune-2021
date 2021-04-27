644c45f979c540d03c66148bc2d0761782cdc111
velocitypaul <68206388+velocitypaul@users.noreply.github.com>
2021-04-27 02:35:46 +0000

    Split answer and begin loadPuzzle function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import { data } from './js/data.js';
 import Game from './js/game.js';
 import Player from './js/player.js';
-import Puzzle from './js/puzzle.js';
+//import Puzzle from './js/puzzle.js';
 import './css/base.scss';
 import './images/wheel_of_fortune_small_logo.png';
 import './images/wheel_fortune_circle.png';
@@ -18,35 +18,44 @@ var correctAnswer;
 var game;
 
 window.onload = function() {
-	getPuzzles();
-	startGame();
-	console.log(mergedPuzzles);
-	console.log(randomPuzzle);
-	console.log(correctAnswer);
-	document.querySelector('body').classList.remove( 'round' + game.round );
-	game.nextRound();
-	document.querySelector('body').classList.add( 'round' + game.round );
+  getPuzzles();
+  startGame();
+  console.log(mergedPuzzles);
+  console.log(randomPuzzle);
+  console.log(correctAnswer);
+  document.querySelector('body').classList.remove( 'round' + game.round );
+  game.nextRound();
+  document.querySelector('body').classList.add( 'round' + game.round );
 }
 
 
 function getPuzzles() {
-	 Object.entries(data.puzzles).forEach( function(puzzleSet) {
-		 var puzzleBank = puzzleSet[1].puzzle_bank;
-		 mergedPuzzles.push(puzzleBank);
-	 });
-	 return mergedPuzzles = mergedPuzzles.flat(2);	 
+  Object.entries(data.puzzles).forEach( function(puzzleSet) {
+    var puzzleBank = puzzleSet[1].puzzle_bank;
+    mergedPuzzles.push(puzzleBank);
+  });
+  return mergedPuzzles = mergedPuzzles.flat(2);	 
 }
 
-function loadRandomPuzzle() {
-	//var randIndex = 30;
-	var randIndex = Math.floor(Math.random() * mergedPuzzles.length);
-	randomPuzzle = mergedPuzzles[randIndex];
-	correctAnswer = randomPuzzle.correct_answer;
+function getPuzzle() {
+  var randIndex = 30;
+  //var randIndex = Math.floor(Math.random() * mergedPuzzles.length);
+  randomPuzzle = mergedPuzzles[randIndex];
+  correctAnswer = randomPuzzle.correct_answer.split(" ");
+}
+
+function loadPuzzle() {
+  var line1 = correctAnswer[0];
+  var line2 = correctAnswer[1];
+  var line3 = correctAnswer[2];
+  var line4 = correctAnswer[3];
+  console.log(line1);
 }
 
 function startGame() {
-	game = new Game();
-	loadRandomPuzzle();
+  game = new Game();
+  getPuzzle();
+  loadPuzzle();
 	
 }
 
@@ -55,17 +64,17 @@ function startGame() {
 spinButton.addEventListener('click', spinWheel);
 
 function spinWheel() {
-	//spin the wheel
-	//wheelImage.classList.toggle('spinning');
-	//select random index from wheel array
-	var randIndex = Math.floor(Math.random() * data.wheel.length);
-	var randIndexValue = data.wheel[randIndex];
-	//stop wheel at index * 15°
-	var wheelStopLocation = (randIndex * 15) + 360;
-	console.log(wheelStopLocation);
-	//display value
-	wheelAmount.innerText = randIndexValue;
-	wheelImage.animate({ transform: [ 'rotate(0deg)', 'rotate(' + wheelStopLocation + 'deg)' ] },
+  //spin the wheel
+  //wheelImage.classList.toggle('spinning');
+  //select random index from wheel array
+  var randIndex = Math.floor(Math.random() * data.wheel.length);
+  var randIndexValue = data.wheel[randIndex];
+  //stop wheel at index * 15°
+  var wheelStopLocation = (randIndex * 15) + 360;
+  console.log(wheelStopLocation);
+  //display value
+  wheelAmount.innerText = randIndexValue;
+  wheelImage.animate({ transform: [ 'rotate(0deg)', 'rotate(' + wheelStopLocation + 'deg)' ] },
 	 { duration: 1000, iterations: 1 });
 	
 }
